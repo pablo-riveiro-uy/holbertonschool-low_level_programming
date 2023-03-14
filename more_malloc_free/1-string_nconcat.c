b77b07cf48a4c038cdcee5c7f657f77bea6c5221
TheWhiteMachine <priveiro@gmail.com>
2023-03-14 12:51:26 -0700

    auto push

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concat two strings in alloc mem
+ *
+ * Return: pointer with concat NULLfor some is empty or failure
+ * @s1: first string
+ * @s2: second string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0,
+		     j = 0,
+		     len1 = 0,
+		     len2 = 0;
+	char *concatString = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	if (n <= len2)
+		len2 = n;
+	
+
+	concatString = malloc(sizeof(char) * len1 + len2 + 1);
+
+	if (concatString == NULL)
+		return (NULL);
+
+	while (i < len1)
+	{
+		concatString[i] = s1[i];
+		i++;
+	}
+	while (j < len2)
+	{
+		concatString[i] = s2[j];
+		i++;
+		j++;
+	}
+	concatString[i] = '\0';
+	return (concatString);
+}
