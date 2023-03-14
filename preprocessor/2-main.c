a51d1b2f862ae7bdc9f1997e2e03a9e5f81ddbe0
TheWhiteMachine <priveiro@gmail.com>
2023-03-14 16:20:54 -0700

    auto push

---
+++ preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * printFileName - print file of original file.
+  */
+void printFileName(void)
+{
+	printf("$s\n", __FILE__);
+}
