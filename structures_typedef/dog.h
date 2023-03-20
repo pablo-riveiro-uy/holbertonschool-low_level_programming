c0f1a0c8ae3a3b48c049f4eec7b3791bf0896dda
TheWhiteMachine <priveiro@gmail.com>
2023-03-20 05:50:03 -0700

    auto push

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef structures
+#define structures
+
+/**
+ * struct dog - Struct for a dog
+ *@name: char for name
+ @age: float for age
+ @char: owner
+ */
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+#endif
+
+
