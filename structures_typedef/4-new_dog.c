bee5e535b4f1e4749517b6153d4bf18dac792c6b
TheWhiteMachine <priveiro@gmail.com>
2023-03-20 10:29:59 -0700

    auto push

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * dog_t - an instance of other dog
+ *@name: string
+ @age: float to age
+ @owner: strign to a ownwer name
+ * Return: Always 0.
+ */
+int dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *nameCopy;
+	char *ownerCopy;
+
+	if (new_dog == NULL)
+		return(NULL)
+	else
+	{
+		struct new_dog my_dog;
+		my_dog Dog_2;
+		nameCopy = malloc(sizeof(name));
+		ownerCopy = malloc(sizeof(owner));
+		if (nameCopy == NULL)
+			return(NULL)
+		if (ownerCopy == NULL)
+			return(NULL);
+		nameCopy->name;
+		ownerCopy->owner;
+	}
+	return(0);
+}
+
