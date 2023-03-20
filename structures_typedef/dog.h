c63bd4c753e18156be8d0803c462d93b6f38b038
TheWhiteMachine <priveiro@gmail.com>
2023-03-20 09:52:06 -0700

    auto push

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,6 +16,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
 #endif
 
 
