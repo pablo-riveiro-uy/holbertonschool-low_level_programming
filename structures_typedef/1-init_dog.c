64ece1e37d64070b9ea120b41e5c0506e50f8782
TheWhiteMachine <priveiro@gmail.com>
2023-03-20 06:34:45 -0700

    auto push

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a dog structure
+ * @d: pointer to dogstructure
+ * @name: char for dog nam
+ * @age: float for dog age
+ * @owner: char for owner name
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
