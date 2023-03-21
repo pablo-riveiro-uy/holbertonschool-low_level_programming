0548a8a36ec9089eb02ae423513017263ad14cd3
TheWhiteMachine <priveiro@gmail.com>
2023-03-21 12:24:42 -0700

    auto push

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an integer
+ * @array: array of int
+ * @size: len of array
+ * @cmp: function to execute
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int res;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		res = cmp(array[i]);
+		if (res != 0)
+			return (i);
+	}
+	return (-1);
+}
