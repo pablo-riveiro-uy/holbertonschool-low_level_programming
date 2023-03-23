463ae514448736e5c60541755b9d694f449bb39c
TheWhiteMachine <priveiro@gmail.com>
2023-03-23 12:13:56 -0700

    auto push

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_strings - print n strings
+ * @n: n args to be printed
+ * @separator: char to separate
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("nill");
+		else
+			printf("%s", str);
+
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf ("\n");
+	va_end(args);
+
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef variadics
 #define variadics
+
 void print_strings(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
