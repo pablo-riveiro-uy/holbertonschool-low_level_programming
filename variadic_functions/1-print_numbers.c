53241df65069a13d4162efabdb2e46fb9e5ec57a
TheWhiteMachine <priveiro@gmail.com>
2023-03-23 11:00:27 -0700

    auto push

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * Print_numbers - print n numbers with separator character
+ * @n: n fot n numbers to print
+ * @separator: separation character
+ * Return: list of numbers or NULL if no separator
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+	unsigned int i = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef variadics
 #define variadics
-
+void print_strings(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
