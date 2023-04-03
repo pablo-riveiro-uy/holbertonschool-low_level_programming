8dc8942c6612bf8d953044aa157c9ac450c9cd24
TheWhiteMachine <priveiro@gmail.com>
2023-04-03 10:27:37 -0700

    nill by nil fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,14 +8,14 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
 	while (h != NULL)
 	{
 		if (h->str != NULL)
 			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[0] (nill)\n");
+			printf("[0] (nil)\n");
 		h = h->next;
 		count++;
 	}
