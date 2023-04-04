aad5a5d092358c5548d08595243709762970a0df
TheWhiteMachine <priveiro@gmail.com>
2023-04-04 13:08:30 -0700

    null on head case try

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,7 +14,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		new_element = malloc(sizeof(dlistint_t));
 		if (new_element == NULL)
 			return (NULL);
-		if (head == 0)
+
+		if (head == NULL)
 			return (NULL);
 
 		new_element->n = n;
