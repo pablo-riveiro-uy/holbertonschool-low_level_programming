d6f92f8e5eb400ae1fe1623272d546bec2734b4f
TheWhiteMachine <priveiro@gmail.com>
2023-04-04 13:04:26 -0700

    case null on head task 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * add_node_end - add a node at the end of list
+ * add_dnodeint_end - add a node at the end of list
  * @head: actual head of the structure
- * @str: string data to new node to add
+ * @n: string data to new node to add
  * Return: the new list or NULL
   */
 
@@ -13,6 +13,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 		new_element = malloc(sizeof(dlistint_t));
 		if (new_element == NULL)
+			return (NULL);
+		if (head == 0)
 			return (NULL);
 
 		new_element->n = n;
