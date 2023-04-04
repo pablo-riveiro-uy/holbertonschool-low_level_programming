2d140a5b2aa20c3c2549147663d2c82d8cf068a2
TheWhiteMachine <priveiro@gmail.com>
2023-04-04 09:46:01 -0700

    task1 finding null case

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,17 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_element;
+	if (head != NULL)
+	{
+		new_element = malloc(sizeof(dlistint_t));
+		if (new_element == NULL)
+			return (NULL);
 
-	new_element = malloc(sizeof(dlistint_t));
-	if (new_element == NULL)
-		return (NULL);
-
-	new_element->n = n;
-	new_element->next = *head;
-	*head = new_element;
-	return (new_element);
+		new_element->n = n;
+		new_element->next = *head;
+		*head = new_element;
+		return (new_element);
+	}
+	return (0);
 }
+
