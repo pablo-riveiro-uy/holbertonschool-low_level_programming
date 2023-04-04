61c183786a96c830c8230d0bae96ee1cb6717c9b
TheWhiteMachine <priveiro@gmail.com>
2023-04-04 14:44:56 -0700

    null case try 2

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,29 +11,25 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_element, *aux;
 
-		new_element = malloc(sizeof(dlistint_t));
-		if (new_element == NULL)
-			return (NULL);
+	new_element = malloc(sizeof(dlistint_t));
+	if (new_element == NULL)
+		return (NULL);
 
-		if (head == NULL)
-			return (NULL);
-
-		new_element->n = n;
-		new_element->next = NULL;
-
-		if (*head == NULL)
-		{
-			*head = new_element;
-			return (*head);
-		}
-		else
-		{
-			aux = *head;
-			while (aux && aux->next)
-				aux = aux->next;
-
-			aux->next = new_element;
-			return (aux);
-		}
+	new_element->n = n;
+	new_element->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_element;
+		return (*head);
+	}
+	else
+	{
+		aux = *head;
+		while (aux && aux->next)
+			aux = aux->next;
+		new_element->prev = aux;
+		aux->next = new_element;
+		return (new_element);
+	}
 	return (NULL);
 }
