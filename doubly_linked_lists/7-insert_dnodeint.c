266327194954722d9d2bf152b28eb8a120e44e36
TheWhiteMachine <priveiro@gmail.com>
2023-04-06 11:04:28 -0700

    empty case still trying and case with idx 0

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx > len)
 		return (NULL);
 
-	if (idx == 0 || *h == NULL)
+	if (idx == 0 && *h == NULL)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
