74db3e9e2c79203d7180a43fa0b22c5a83964efe
TheWhiteMachine <priveiro@gmail.com>
2023-04-07 14:18:14 -0700

    task 1

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,8 +1,9 @@
 #ifndef _bitM
 #define _bitM
 #include <stdio.h>
-
+#include <unistd.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
