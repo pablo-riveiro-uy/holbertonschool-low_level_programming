9c3fa287dc6d235a303609cbd5b647a00f26b8ad
TheWhiteMachine <priveiro@gmail.com>
2023-04-07 11:39:55 -0700

    null case again

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,22 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
 	int len, pot = 1;
-
-	if (*b == '\0' || b == NULL)
-		return (0);
-
-	for (len = 0; b[len];)
-		len++;
-	for (len -= 1; len >= 0; len--)
+	if (b != NULL)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*b == '\0')
 			return (0);
-		res += (b[len] - '0') * pot;
-		pot *= 2;
+
+		for (len = 0; b[len];)
+			len++;
+		for (len -= 1; len >= 0; len--)
+		{
+			if (b[len] != '0' && b[len] != '1')
+				return (0);
+			res += (b[len] - '0') * pot;
+			pot *= 2;
+		}
+		return (res);
 	}
-	return (res);
+	return (0);
+
 }
