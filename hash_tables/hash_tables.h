1f841cf4d6c301ee7fa2dcf0db48e8a0e0988fe7
TheWhiteMachine <priveiro@gmail.com>
2023-04-13 12:20:00 -0700

    more betty

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -29,7 +29,7 @@ typedef struct hash_node_s
 typedef struct hash_table_s
 {
 	unsigned long int size;
-	hash_node_t **array;		  
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
