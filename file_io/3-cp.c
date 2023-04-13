74d34e7d89bfda926bb61a7ffffb2e50e28000df
TheWhiteMachine <priveiro@gmail.com>
2023-04-13 09:56:54 -0700

    cccccc

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,25 +18,34 @@ int main(int ac, char *av[])
 	fo = open(av[1], O_RDONLY);
 
 	if (fo == -1 || fo == 0 || !fo)
+	{
+		close(fo);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
+	}
 	foLen = read(fo, buff,  1024);
-	if (fo > 1024)
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]), exit(99);
-
+	if (fo > foLen)
+	{
+		close(fo);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	}
 	if (foLen == -1 || foLen == 0)
+	{
+		close(fo);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-
+	}
 	fd = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 664);
 
 	if (fd == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write ot file %ld\n", fd), exit(99);
-
-	cp = write(fd, buff, foLen);
-
-	if (cp < fo || cp == -1)
+	{
+		close(fd);
 		dprintf(STDERR_FILENO, "Error: Can't write to file %ld\n", fd), exit(99);
-	
+	}
+	cp = write(fd, buff, foLen);
+	if (cp == -1)
+	{
+		close(fd);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %ld\n", fd), exit(99);
+	}
 	if (close(fo) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fo), exit(100);
 	if (close(fd) == -1)
