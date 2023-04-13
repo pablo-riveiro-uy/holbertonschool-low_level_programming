4362abff10770182d2ffb8620a43a41e7f77f571
TheWhiteMachine <priveiro@gmail.com>
2023-04-13 08:12:24 -0700

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @ac: arguments counter
+ * @av: vector of arguments
+ * Return: Always 0.
+ */
+int main(int ac, char *av[])
+{
+	char *buff[1024];
+	ssize_t fo, foLen, fd, cp;
+	int len1 = 0;
+
+	if (ac < 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	while (av[1][len1])
+		len1++;
+
+	fo = open(av[1], O_RDONLY);
+
+	if (fo == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	foLen = read(fo, buff, 1024);
+
+	if (foLen == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	fd = open(av[2], O_CREAT | O_RDWR | O_TRUNC, 664);
+
+	cp = write(fd, buff, foLen);
+
+	if (cp == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write ot file %s\n", av[2]), exit(99);
+	if (close(fo) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fo), exit(100);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd), exit(100);
+	return (0);
+}
