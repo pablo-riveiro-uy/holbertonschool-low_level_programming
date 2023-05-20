e1688172af30f8f5da8a0061854fe5c020130d51
TheWhiteMachine <priveiro@gmail.com>
2023-05-20 14:20:46 -0700

    1 task

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - do a linear search on a array
+ * binary_search - do a binary  search on a array
  * @array: vector of ints
  * @size: size of the array
  * @value: value to search for on array
@@ -10,79 +10,30 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    int found;
+	int low = 0, midle;
+	int high = size - 1;
+	int i;
 
-    if (!array || size == 0 || !value)
-        return(-1);
-
-    found = binary_recursion(array, size, 0, size, value);
-
-    printf("found at END %d\n", found);
-
-    if (array[found] == value)
-        return (found);
-
-    return (-1);
-}
-
-int binary_recursion(int *array, size_t size, size_t LOW, size_t HIGH, int value)
-{
-    size_t m_index = (HIGH - LOW) / 2;
-    
-    print_array(array, LOW, HIGH);
-
-    printf("low %ld, hight %ld, numARRRAY %d \n", LOW, HIGH, array[m_index]);
-
-    if (m_index != '\0')
-    {
-        if (array[m_index] == value)
-        {
-            puts("encontrado !!!!\n");
-            printf("index %ld, contenido %d\n", m_index, array[m_index]);
-            return (m_index);
-        }
-        m_index = (HIGH - LOW) /2;
-    }else
-        m_index = (HIGH - LOW) /2;
-
-     if (array[m_index] == value)
-        return (m_index);
-
-
-
-    if (array[m_index] > value)
-    {
-        LOW = 0;
-        HIGH = m_index - 1;
-        if (LOW < HIGH)
-            binary_recursion(array, size, LOW, HIGH, value);
-    }
-    else
-    {
-        if (array[m_index] < value)
-        {
-            LOW = m_index + 1;
-            HIGH = size;
-            if (LOW < HIGH)
-                binary_recursion(array, size, LOW, HIGH, value);
-        }
-    }
-
-    return (m_index);
-
-
-
-    return(m_index);
-}
-
-void print_array(int *array, size_t LOW, size_t HIGH)
-{
-        printf("Searching in array: ");
-    	for (; LOW <= HIGH -1; LOW++)
-        {
-            printf("%d", array[LOW]);
-            if (LOW != HIGH - 1)
-                printf(", ");
-        }
-        printf("\n");
+	if (array)
+	{
+		while (low < high)
+		{
+			printf("Searching in array: ");
+			for (i = low; i <= high; i++)
+			{
+				printf("%d", array[i]);
+				if (i < high)
+					printf(", ");
+			}
+			putchar('\n');
+			midle = low + (high - low) / 2;
+			if (array[midle] == value)
+				return (midle);
+			else if (array[midle] < value)
+				low = midle + 1;
+			else
+				high = midle - 1;
+		}
+	}
+	return (-1);
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -7,7 +7,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t LOW, size_t HIGH);
-int binary_recursion(int *array, size_t size, size_t LOW, size_t HIGH, int value);
+
 
 
 
